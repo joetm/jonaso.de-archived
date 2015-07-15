a5be270269275f9b62e98701c7e1016e32c7a291
Jonas O <jonaso@web.de>
2015-07-15 18:01:14 +0100

    more testing setup

--- karma.conf.js
+++ karma.conf.js
@@ -5,13 +5,11 @@ module.exports = function(config) {
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
-
+    basePath: './',
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine'],
-
 
     // list of files / patterns to load in the browser
     files: [
@@ -32,17 +30,29 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+        'prod/js/*.js': ['coverage'],
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: [
+        'progress',
+        'coverage'
+    ],
 
+    //see https://github.com/karma-runner/karma-coverage
+    coverageReporter: {
+        type : 'lcov',
+        dir : 'test/coverage/',
+        subdir: function (browser) {
+            return browser.toLowerCase().split(/[ /-]/)[0];
+        }
+    },
 
     // web server port
-    port: 9876,
+    port: 9875,
 
 
     // enable / disable colors in the output (reporters and logs)
@@ -60,11 +70,13 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Firefox'],
-
+    browsers: [
+        'Firefox'
+    ],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: false
-  })
+
+  });
 }
--- test/app-Spec.js
+++ test/app-Spec.js
@@ -10,5 +10,40 @@ describe('app', function() {
         expect(countries).toBeDefined();
     });
 
+    it('defines countries array', function() {
+        expect(countries).toBeDefined();
+    });
+
+
+    describe('contains function', function() {
+
+        it('is defined', function() {
+            expect(contains).toBeDefined();
+            expect(typeof contains).toEqual('function');
+        });
+
+        it('returns true if array contains object', function() {
+            var obj = {
+                my: 'attribute'
+            };
+            var array = [1, 2, obj, 3];
+            //test the function
+            var ret = contains(array, obj);
+            //
+            expect(ret).toEqual(true);
+        });
+
+        it('returns false if array does not contain object', function() {
+            var obj = {
+                my: 'attribute'
+            };
+            var array = [1, 2, 3];
+            //test the function
+            var ret = contains(array, obj);
+            //
+            expect(ret).toEqual(false);
+        });
+
+    });
 
 });//describe
