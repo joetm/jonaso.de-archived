bb8952fabb03e08084ce6b4b574c941d42fbbae4
Jonas O <jonaso@web.de>
2015-07-15 17:27:53 +0100

    karma coverage

--- karma.conf.js
+++ karma.conf.js
@@ -32,12 +32,26 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+        'frontend/js/*.js': ['coverage'],
     },
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: [
+        'progress',
+        'coverage'
+    ],
+
+    //see https://github.com/karma-runner/karma-coverage
+    coverageReporter: {
+        type : 'lcov',
+        dir : 'test/coverage/',
+        //subdir: '.' //output the results into: 'test/coverage/'
+        subdir: function (browser) {
+            return browser.toLowerCase().split(/[ /-]/)[0];
+        }
+    },
 
     // web server port
     port: 9876,
